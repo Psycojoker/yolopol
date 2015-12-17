3ee4785aec7e08a4e5db672fcacf2f7a0a34c428
jpic <jamespic@gmail.com>
2015-12-17 17:01:58 +0100

    Test position

--- positions/tests.py
+++ positions/tests.py
@@ -1,2 +1,27 @@
+from django.test import TestCase, RequestFactory, Client
+from django.contrib.auth.models import AnonymousUser
 
-# Create your tests here.
+from legislature.models import MemopolRepresentative
+from positions.models import Position
+
+# position-create
+# position-detail(pk)
+
+class PositionCreateTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_create_position(self):
+        client = Client()
+        mep = MemopolRepresentative.objects.create(
+                full_name='PositionCreateTest test_create_position',
+                slug='PositionCreateTest-test_create_position')
+
+        import ipdb; ipdb.set_trace()
+        client.post('/positions/create', {
+            'tags': 'foo,bar',
+            'datetime': '2015-12-11',
+            'text': 'bla',
+            'link': 'http://example.com/bar',
+            'representative': mep.pk
+        })
